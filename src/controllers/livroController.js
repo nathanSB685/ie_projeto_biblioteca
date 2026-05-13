2aa91e646c6942c1f763b91c5c158470b914267a
Nathan <nathansbarros2005@gmail.com>
2026-05-13 11:41:14 -0300

    feat: rota para deletar livro adicionada (somente bibliotecario)

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -36,4 +36,33 @@ const listarLivros = async (req, res) => {
   }
 };
 
-module.exports = { cadastrarLivro, listarLivros };
+const removerLivro = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM livros WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ erro: "Livro não encontrado." });
+    }
+
+    res
+      .status(200)
+      .json({ mensagem: "Livro removido do catálogo com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao remover livro:", error);
+
+    // O erro 23503 no PostgreSQL significa "Violação de Chave Estrangeira"
+    if (error.code === "23503") {
+      return res.status(400).json({
+        erro: "Não é possível excluir este livro porque existe um histórico de empréstimos vinculado a ele.",
+      });
+    }
+    res.status(500).json({ erro: "Erro interno ao tentar remover o livro." });
+  }
+};
+
+module.exports = { cadastrarLivro, listarLivros, removerLivro };
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -1,13 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const livroController = require('../controllers/livroController');
-const { verificarToken, apenasBibliotecario } = require('../middlewares/authMiddleware');
+const livroController = require("../controllers/livroController");
+const {
+  verificarToken,
+  apenasBibliotecario,
+} = require("../middlewares/authMiddleware");
 
 // ROTA PROTEGIDA: api/livros
 // passa pelo verificador de token, verificador de perfil, e só então chega no cadastro
-router.post('/livros', verificarToken, apenasBibliotecario, livroController.cadastrarLivro);
+router.post(
+  "/livros",
+  verificarToken,
+  apenasBibliotecario,
+  livroController.cadastrarLivro
+);
 
 // Rota de LISTAGEM (Protegida: Requer apenas Token de quem está logado)
-router.get('/livros', verificarToken, livroController.listarLivros);
+router.get("/livros", verificarToken, livroController.listarLivros);
+
+// Rota para deletar livro (Note o DELETE e a proteção dupla)
+router.delete(
+  "/livros/:id",
+  verificarToken,
+  apenasBibliotecario,
+  livroController.removerLivro
+);
 
 module.exports = router;
